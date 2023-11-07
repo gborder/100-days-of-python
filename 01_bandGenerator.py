bbe334b4e4d45ac13475e2f075a700c54a6a0700
Gustavo Border <gustavo.hs.border@gmail.com>
2023-11-07 14:59:25 +0000

    Added the day 1 project
    Very simple and easy project to learn python basics and console prompts.
    A band name generator was born.

---
+++ 01_bandGenerator.py
@@ -0,0 +1,4 @@
+print("Hello there and welcome to the Band Name Generator\n")
+verb_ing = input("Please type a verb in present participle (like running): \n")
+animal = input("Please type a name of an animal: \n")
+print("The name of your kick as band is: The " + verb_ing + " " + animal)
