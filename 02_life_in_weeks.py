0e1c6f1238487f7b77066834e1c51e13f4a2a9d4
Gustavo Border <gustavo.hs.border@gmail.com>
2023-11-07 16:35:23 +0000

    A very depressing one
    Is the "how many weeks you have left" project.
    But is pretty nice.

---
+++ 02_life_in_weeks.py
@@ -0,0 +1,7 @@
+# Your life in weeks
+print("Wanna check how many weeks left for you to live? ")
+age = input("Please let me know your age: \n")
+years = 90 - int(age)
+weeks = years * 52
+print("You have left " + str(weeks) + " to live in your life")
+print("Please enjoy everyday as it was the last.")
